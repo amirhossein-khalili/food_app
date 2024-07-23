c72a647209deb14ef4b839576812f02fb5c8ee46
amirkhalili <amir1378khalili@gmail.com>
2024-07-23 20:47:11 +0330

    ADD password and notification utills

---
+++ src/utils/Notification.Util.ts
@@ -0,0 +1,40 @@
+import { Kavenegar_API_KEY, Kavenegar_PHONE_NUMBER } from '../config';
+import { Kavenegar } from 'kavenegar';
+
+/* ------------------- Email --------------------- */
+
+/* ------------------- Notification --------------------- */
+
+/* ------------------- OTP --------------------- */
+
+export const GenerateOtp = () => {
+  const otp = Math.floor(10000 + Math.random() * 900000);
+  let expiry = new Date();
+  expiry.setTime(new Date().getTime() + 30 * 60 * 1000);
+
+  return { otp, expiry };
+};
+
+export const onRequestOTP = async (otp: number, toPhoneNumber: string) => {
+  try {
+    const client = Kavenegar.KavenegarAPI({
+      apikey: Kavenegar_API_KEY,
+    });
+
+    const params = {
+      sender: Kavenegar_PHONE_NUMBER,
+      receptor: toPhoneNumber,
+      message: `Your OTP is ${otp}`,
+    };
+
+    const response = client.Send(params);
+
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
+
+/* ------------------- Payment --------------------- */
+
+/* ------------------- SMS --------------------- */
