8a16f4712affd20cbb72e5734f72eaf297feb14a
amirkhalili <amir1378khalili@gmail.com>
2024-07-24 01:40:28 +0330

    ADD CustomerRoute to app

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import compression from 'compression';
 import cors from 'cors';
 import { json, urlencoded } from 'express';
+import { CustomerRoute } from '../api/v1/routes';
 
 export default async (app: Application): Promise<Application> => {
   /*
@@ -22,6 +23,8 @@ export default async (app: Application): Promise<Application> => {
   app.use(json());
   app.use(urlencoded({ extended: true }));
   app.use(json());
+
+  app.use('/api/v1/customer', CustomerRoute);
 
   // Middleware for error handling (should be placed after all other app.use() calls)
   app.use((err, req, res, next) => {
