31a41d3e43b259e3b391c5c7691393afbaa5304c
amirkhalili <amir1378khalili@gmail.com>
2024-07-24 23:54:40 +0330

    REMOVE extra items from startServer

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import express from 'express';
 import App from './services/ExpressApp';
 import dbConnection from './services/Database';
 import { PORT } from './config';
-import fs from 'fs';
-import expressListEndpoints from 'express-list-endpoints';
-import path from 'path';
 
 const startServer = async () => {
   const app = express();
@@ -12,26 +9,7 @@ const startServer = async () => {
   try {
     await dbConnection();
     await App(app);
-    /*
-    
-    
-    
-    
-    */
-    const endpoints = expressListEndpoints(app);
-    const csvFilePath = path.join(__dirname, 'endpoints.csv');
-    const csvContent = endpoints
-      .map((route) => {
-        return `${route.path},${route.methods.join(',')}`;
-      })
-      .join('\n');
-    fs.writeFileSync(csvFilePath, 'Path,Methods\n' + csvContent);
-    /*
- 
- 
- 
- 
- */
+    ``;
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
