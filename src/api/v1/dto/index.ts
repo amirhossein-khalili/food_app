985ec98b1ed644cf9c6fc63ce0abd857998f927e
amirkhalili <amir1378khalili@gmail.com>
2024-07-24 23:45:52 +0330

    ADD order dto

--- src/api/v1/dto/index.ts
+++ src/api/v1/dto/index.ts
@@ -2,4 +2,5 @@ export * from './Vandor.dto';
 export * from './Auth.dto';
 export * from './Food.dto';
 export * from './Customer.dto';
-export * from './payment.dto'
+export * from './payment.dto';
+export * from './Order.dto';
